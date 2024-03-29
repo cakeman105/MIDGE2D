e8f0d0bc82be7b2e3b5f235304e189f0f4801d1d
Joshua David Crofts <croftjos@fel.cvut.cz>
2024-03-29 16:12:07 +0100

    Redo project

---
+++ MIDGE2D/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module cz.cvut.fel.pjv.midge2d {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+
+    opens cz.cvut.fel.pjv.midge2d to javafx.fxml;
+    exports cz.cvut.fel.pjv.midge2d;
+}
